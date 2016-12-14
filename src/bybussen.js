6b90276612a52902808b7aad84e2949a41f8c7e8
Tri Nguyen <mail@trimn.net>
2016-12-14 12:49:02 +0100

    Rename server.js -> bybussen.js

--- src/bybussen.js
+++ src/bybussen.js
@@ -1,11 +1,12 @@
 const express = require('express')
+, haversine = require('./haversine')
 , http = require('http')
 , https = require('https')
+, log  = require('./logger')
+, stops = require('./data/stops').stops
 , xml = require('xml2js').parseString
 
-const log  = require('./log')
 const app = express()
-const stops = require('../data/stops').stops
 
 const extract_data =  (data, cb) => {
   xml(data, (err, res) => {
@@ -46,25 +47,6 @@ const extract_data =  (data, cb) => {
   })
 }
 
-
-//+-----------------------------------------------------------------------------
-//
-// Haversine algorithm
-//
-//------------------------------------------------------------------------------
-const to_rad = (num) => num * Math.PI / 180
-
-const haversine = (lat1, lon1, lat2, lon2, unit) => {
-  const R = 6371000
-
-  const φ1 = to_rad(lat1)
-  , φ2 = to_rad(lat2)
-  , Δλ = to_rad(lon2 - lon1)
-
-  const distance = Math.acos(Math.sin(φ1) * Math.sin(φ2) + Math.cos(φ1) * Math.cos(φ2) * Math.cos(Δλ)) * R
-
-  return distance
-}
 
 //+-----------------------------------------------------------------------------
 //
